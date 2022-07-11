3836f4ebafa4fa93b4d4adfc5e1be93b37eb0327
Dante <dante_lee@calcomp.com.tw>
2022-07-11 17:16:13 +0800

    fix

--- websocket/src/main/java/com/dante/ws/ctrl/DevTools.java
+++ websocket/src/main/java/com/dante/ws/ctrl/DevTools.java
@@ -1,6 +1,5 @@
 package com.dante.ws.ctrl;
 
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -11,18 +10,9 @@ import org.springframework.web.bind.annotation.RestController;
 		produces = MediaType.TEXT_HTML_VALUE)
 public class DevTools extends BaseController {
 
-	@Value("${version}")
-	private String version;
-
 	@RequestMapping(value = "/hello",
 			method = RequestMethod.GET)
 	public String hello() throws Exception {
 		return "OK";
-	}
-
-	@RequestMapping(value = "/version",
-			method = RequestMethod.GET)
-	public String version() throws Exception {
-		return version;
 	}
 }
