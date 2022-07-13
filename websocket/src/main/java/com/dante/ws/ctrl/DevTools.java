f109b43abb08a0bd45f1991615566a4ef220f89b
Dante <dante_lee@calcomp.com.tw>
2022-07-13 15:40:37 +0800

    Recover version function

--- websocket/src/main/java/com/dante/ws/ctrl/DevTools.java
+++ websocket/src/main/java/com/dante/ws/ctrl/DevTools.java
@@ -1,5 +1,6 @@
 package com.dante.ws.ctrl;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -10,9 +11,18 @@ import org.springframework.web.bind.annotation.RestController;
 		produces = MediaType.TEXT_HTML_VALUE)
 public class DevTools extends BaseController {
 
+	@Value("${version}")
+	private String version;
+
 	@RequestMapping(value = "/hello",
 			method = RequestMethod.GET)
 	public String hello() throws Exception {
 		return "OK";
+	}
+
+	@RequestMapping(value = "/version",
+			method = RequestMethod.GET)
+	public String version() throws Exception {
+		return version;
 	}
 }
